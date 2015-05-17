03e848eb6e245be0b919d43b8f8415f0a94ecef3
Vasa <vasanth.krishnamoorthy@citrix.com>
2015-05-17 12:01:19 -0500

    ES6 Examples: Object Literal shorthand - More examples

--- object-literal.js
+++ object-literal.js
@@ -1,6 +1,7 @@
 /** Objext Literal
  * While every object property needs to be either a key-value pair or getter/setter, this may change in the near future.
  * Another syntactic sugar in the upcoming ECMAScript 6 is the object literal property value shorthand.
+ * Ref: http://codepen.io/bradleyboy/posts/getting-to-know-es6-object-literals
  */
 
 // ES5
@@ -18,3 +19,28 @@ function createVillain(name, power) {
 function createHero(name) {
     return {type: 'Luke', name};
 }
+
+// ES5
+var x = 10;
+var y = 30;
+var coordinates = {
+    x: x,
+    y: y
+};
+console.log(coordinates); // { x: 10, y: 30 }
+
+// ES6
+let x = 10;
+let y = 30;
+let coordinates = {x, y};
+console.log(coordinates); // { x: 10, y: 30 }
+
+// ES6
+let x = 10;
+let y = 30;
+let coordinates = {
+    x,
+    y,
+    z: 10
+};
+console.log(coordinates); // { x: 10, y: 30, z: 10 }
